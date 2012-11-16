c60c826e9146831f4138a3cfaa1013f43bdcfbf1
steff <steff@steff>
2012-11-16 16:14:35 +0100

    set under construction

--- Set.java
+++ Set.java
@@ -5,10 +5,50 @@ import java.util.Iterator;
  *
  * @author guetar
  */
-public class Set implements Iterable {
+public abstract class Set<S> implements Iterable<S> {
+    private S head;
 
-    @Override
-    public Iterator iterator() {
-        throw new UnsupportedOperationException("Not supported yet.");
+    private class Node {
+        private S elem;
+        private Node next;
+        
+        private Node(S elem) {
+            this.elem = elem;
+            this.next = null;
+        }
+        
+        private void insert(S elemIns) {
+            if (next != null) {
+                next.insert(elemIns);
+            }
+            else {
+                next = new Node(elemIns);
+            }
+        }
+    }
+    
+    private class MyIterator implements Iterator<S> {
+        private Node pos = null;
+
+        @Override
+        public boolean hasNext() {
+            throw new UnsupportedOperationException("Not supported yet.");
+        }
+
+        @Override
+        public S next() {
+            throw new UnsupportedOperationException("Not supported yet.");
+        }
+
+        @Override
+        public void remove() {
+            throw new UnsupportedOperationException("Not supported yet.");
+        }
+        
+    }
+    
+ 
+    public boolean insert(S s) {
+        
     }
 }
