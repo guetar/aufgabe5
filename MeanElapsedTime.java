129e1746c546bbfaff9157a8a30f8696cd618f7f
steff <steff@steff>
2012-11-17 14:30:45 +0100

    done things

--- MeanElapsedTime.java
+++ MeanElapsedTime.java
@@ -1,29 +1,29 @@
-
-/**
- *
- * @author guetar
- */
-public class MeanElapsedTime<S extends MeanElapsedTime> extends ElapsedTime {
-    
-    private Set<Double> set;
-    
-    public boolean shorter(S s) {
-        return (int) Math.round(avg()) < (int) Math.round(s.avg());
-    }
-    
-    public void add(double t) {
-        set.add(t);
-    }
-    
-    public int count() {
-        return set.length;
-    }
-    
-    public double max() {
-        return 1.0;
-    }
-    
-    public double avg() {
-        return 1.0;
-    }
-}
+//
+///**
+// *
+// * @author guetar
+// */
+//public class MeanElapsedTime<S extends MeanElapsedTime> extends ElapsedTime {
+//    
+//    private Set<Double> set;
+//    
+//    public boolean shorter(S s) {
+//        return (int) Math.round(avg()) < (int) Math.round(s.avg());
+//    }
+//    
+//    public void add(double t) {
+//        set.add(t);
+//    }
+//    
+//    public int count() {
+//        return set.length;
+//    }
+//    
+//    public double max() {
+//        return 1.0;
+//    }
+//    
+//    public double avg() {
+//        return 1.0;
+//    }
+//}
--- OrderedSet.java
+++ OrderedSet.java
@@ -1,20 +1,20 @@
-
-/**
- *
- * @author guetar
- */
-public class OrderedSet<S extends Shorter> extends Set {
-    private OrderedNode head;
-    
-    public Iterator iterator() {
-        
-    }
-    
-    public boolean insert(S s) {
-        if(head == null) {
-            head = new Node(s);
-        } else {
-            head.insert(s);
-        }
-    }
-}
+//
+///**
+// *
+// * @author guetar
+// */
+//public class OrderedSet<S extends Shorter> extends Set {
+//    private OrderedNode head;
+//    
+//    public Iterator iterator() {
+//        
+//    }
+//    
+//    public boolean insert(S s) {
+//        if(head == null) {
+//            head = new Node(s);
+//        } else {
+//            head.insert(s);
+//        }
+//    }
+//}
