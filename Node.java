1fab7be41c70cd57927006a3ef4ab79ed9c01a78
steff <steff@steff>
2012-11-17 14:38:54 +0100

    done things

--- Node.java
+++ Node.java
@@ -8,7 +8,7 @@ public class Node<S> {
     private S elem;
     private Node<S> next;
     private Node<S> prev;
-    
+
     public Node() {
         elem = null;
         next = null;
@@ -28,30 +28,28 @@ public class Node<S> {
             next.setPrev(this);
         }
     }
-    
+
     public boolean hasNext() {
         return (next != null);
     }
-    
+
     public Node<S> getNext() {
         return next;
     }
-    
+
     public void setNext(Node<S> nextN) {
         next = nextN;
     }
-    
+
     public Node<S> getPrev() {
         return prev;
     }
-    
+
     private void setPrev(Node<S> prevN) {
         prev = prevN;
     }
-    
+
     public S getElem() {
         return elem;
     }
-
-   
 }
